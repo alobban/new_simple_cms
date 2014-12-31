d0116f1ee775fa2fde92e49ef18bb192ca502bd6
Andrew Lobban <andrewglobban@gmail.com>
2014-12-31 11:00:27 -0600

    Adds custom validations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -8,6 +8,8 @@ class AdminUser < ActiveRecord::Base
   has_many :sections, through: :section_edits
 
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+FORBIDDEN_USERNAMES = ['littlebopeep','humptydumpty','marymary']
+
   # validates_presence_of :first_name
   # validates_length_of :first_name, :maximum => 25
   # validates_presence_of :last_name
@@ -25,5 +27,22 @@ class AdminUser < ActiveRecord::Base
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :username, length: { within: 8..25 }, uniqueness: true
   validates :email, presence: true, length: { maximum: 100 }, format: EMAIL_REGEX, confirmation: true
+
+  validate :username_is_allowed
+  # validate :no_new_users_on_wednesday, on: :create
+
+  def username_is_allowed
+  	if FORBIDDEN_USERNAMES.include?(username)
+  		errors.add(:username, "has been restricted from use.")
+  	end
+  end
+
+  # Errors not related to a specific attribute
+  # can be added to errors[:base]
+  def no_new_users_on_wednesday
+  	if Time.now.wday == 3
+  		errors[:base] << "No new users on Wednesday."
+  	end
+  end
 
 end

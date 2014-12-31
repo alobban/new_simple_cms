b2e1fab1408029a2ce7fb2573be46eb7cc72b1ed
Andrew Lobban <andrewglobban@gmail.com>
2014-12-31 11:53:48 -0600

    Adds Blowfish - bcrypt

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,12 +3,14 @@ class AdminUser < ActiveRecord::Base
   # To configure a different table name:
   # self.table_name = "admin_users"  
 
+  has_secure_password
+
   has_and_belongs_to_many :pages
   has_many :section_edits
   has_many :sections, through: :section_edits
 
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
-FORBIDDEN_USERNAMES = ['littlebopeep','humptydumpty','marymary']
+  FORBIDDEN_USERNAMES = ['littlebopeep','humptydumpty','marymary']
 
   # validates_presence_of :first_name
   # validates_length_of :first_name, :maximum => 25

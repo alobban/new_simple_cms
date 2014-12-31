326ad83e4c87e7aaf91ba83b68170c2c0cd5824b
Andrew Lobban <andrewglobban@gmail.com>
2014-12-31 09:47:26 -0600

    Adds Sexy Validations

--- app/models/section.rb
+++ app/models/section.rb
@@ -6,10 +6,15 @@ class Section < ActiveRecord::Base
 
   CONTENT_TYPES = ['text', 'HTML']
 
-  validates_presence_of :name
-  validates_length_of :name, maximum: 255
-  validates_inclusion_of :content_type, in: CONTENT_TYPES, message: "must be one of: #{CONTENT_TYPES.join(', ')}"
-  validates_presence_of :content
+  # validates_presence_of :name
+  # validates_length_of :name, maximum: 255
+  # validates_inclusion_of :content_type, in: CONTENT_TYPES, message: "must be one of: #{CONTENT_TYPES.join(', ')}"
+  # validates_presence_of :content
+
+  # shortcut validations, aka "sexy validations"
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :content_type, inclusion: { in: CONTENT_TYPES, message: "must be one of: #{CONTENT_TYPES.join(', ')}" }
+  validates :content, presence: true
 
   scope :visible, lambda {where(visible: true)}
   scope :invisible, lambda {where(visible: false)}
